29d0222f8081fe21f36a2a249d4a501b47807a93
“Rivera <em.river4@gmail.com>
2025-07-30 14:28:02 +0200

    Update inhabitant class

--- models/inhabitant.py
+++ models/inhabitant.py
@@ -1,3 +1,10 @@
 class Inhabitant:
 
-    pass
+    """This class represents the inhabitant model of the city"""
+
+    def __init__(self, name, age, profession, happiness=50, has_roof=False):
+        self.name = name
+        self.age = age
+        self.profession = profession
+        self.happiness = happiness
+        self.has_roof = has_roof
