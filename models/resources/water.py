3dc8bc3c63a8779f8a8f432e8400dfbd3f772880
gkonen <guillaumekonen@gmail.com>
2025-07-30 13:55:43 +0200

    add basic logic of resources

--- models/resources/water.py
+++ models/resources/water.py
@@ -1,6 +1,17 @@
 from models.resources.resource import Resource
 
+import random
 
 class Water(Resource):
 
-    pass
+    def __init__(self):
+        # I give arbitrary number where must be changed once the functionnal analyse is done
+        # TODO : Decide the correct value
+        super().__init__(100)
+
+    def collect(self):
+        return random.randint(1, self.max_production)
+
+if __name__ == "__main__":
+    water = Water()
+    print(water.collect())
