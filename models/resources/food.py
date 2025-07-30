49bfaee4f2adc103cc7383df367bcb0d711a5eee
gkonen <guillaumekonen@gmail.com>
2025-07-30 13:55:43 +0200

    add basic logic of resources

--- models/resources/food.py
+++ models/resources/food.py
@@ -1,6 +1,20 @@
 from models.resources.resource import Resource
 
+import random
+
+from models.resources.water import Water
+
 
 class Food(Resource):
 
-    pass
+    def __init__(self):
+        # I give arbitrary number where must be changed once the functionnal analyse is done
+        # TODO : Decide the correct value
+        super().__init__(50)
+
+    def collect(self):
+        return random.randint(1, self.max_production)
+
+if __name__ == "__main__":
+    water = Water()
+    print(water.collect())
--- models/resources/resource.py
+++ models/resources/resource.py
@@ -1,3 +1,16 @@
-class Resource:
+from abc import ABC, abstractmethod
 
-    pass
+class Resource(ABC):
+    """
+        Abstract class which determine the behaviour of the resources
+    """
+
+    def __init__(self, max_production):
+        self.max_production = max_production
+
+    @abstractmethod
+    def collect(self) -> int:
+        """
+            Method which will give the number of resource can be collected by day
+        """
+        pass
