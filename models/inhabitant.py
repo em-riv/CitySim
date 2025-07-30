2ea50db3ea8348ae8af1ce6a83e33c416acb16d3
“Rivera <em.river4@gmail.com>
2025-07-30 14:34:35 +0200

    Update inhabitant class

--- models/inhabitant.py
+++ models/inhabitant.py
@@ -3,8 +3,36 @@ class Inhabitant:
     """This class represents the inhabitant model of the city"""
 
     def __init__(self, name, age, profession, happiness=50, has_roof=False):
-        self.name = name
-        self.age = age
-        self.profession = profession
-        self.happiness = happiness
-        self.has_roof = has_roof
+        self.__name = name
+        self.__age = age
+        self.__profession = profession
+        self.__happiness = happiness
+        self.__has_roof = has_roof
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def age(self):
+        return self.age
+
+    @property
+    def profession(self):
+        return self.profession
+
+    @property
+    def happiness(self):
+        return self.happiness
+
+    @property.getter
+    def happiness(self):
+        return self.happiness
+
+    @property
+    def has_roof(self):
+        return self.has_roof
+
+    @property.getter
+    def has_roof(self):
+        return self.has_roof
