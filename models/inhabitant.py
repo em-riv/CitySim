672a86b0b102cb0e38e07c0de640a7a8addc09ac
“Rivera <em.river4@gmail.com>
2025-07-30 14:26:14 +0200

    Revert "Update of the basic attributes of class inhabitant"
    This reverts commit 50cb179cecd82430d3a91f40254e8bca0b07dd2a.

--- models/inhabitant.py
+++ models/inhabitant.py
@@ -1,11 +1,3 @@
 class Inhabitant:
 
-    """This class represents the inhabitant model of the city"""
-
-    def __init__(self, name, age, profession, happiness=50, has_roof=False):
-        self.name = name
-        self.age = age
-        self.profession = profession
-        self.happiness = happiness
-        self.has_roof = has_roof
-
+    pass
