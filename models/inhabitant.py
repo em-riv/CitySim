467bc6095081928685837183209d6f38d708c7e8
“Rivera <em.river4@gmail.com>
2025-07-30 15:36:39 +0200

    inhabitant update following feedback

--- models/inhabitant.py
+++ models/inhabitant.py
@@ -6,7 +6,7 @@ class Inhabitant:
         self.__name = name
         self.__age = age
         self.__profession = profession
-        self.happiness = happiness
+        self.__happiness = happiness
         self.__has_roof = has_roof
 
     @property
@@ -22,20 +22,29 @@ class Inhabitant:
         return self.__profession
 
     @profession.setter
-    def profession(self, value):
+    def profession(self, value: str):
         self.__profession = value
+
+    @property
+    def happiness(self):
+        return self.__happiness
 
     @property
     def has_roof(self):
         return self.has_roof
 
     @has_roof.setter
-    def has_roof(self, value):
+    def has_roof(self, value: bool):
         self.__has_roof = value
 
-    def gain_happiness(self, value):
-        self.happiness += value
-        return self.happiness
+    def gain_happiness(self, value: int):
+        if self.__happiness + value >= 100:
+            self.__happiness = 100
+        else:
+            self.__happiness += value
 
-    def lose_happiness(self, value):
-        self.happiness -= value
+    def lose_happiness(self, value: int):
+        if self.__happiness - value <= 0:
+            self.__happiness = 0
+        else:
+            self.__happiness -= value
