677ee92d7692da778c6017305c38e623b664102f
“Rivera <em.river4@gmail.com>
2025-07-31 10:31:44 +0200

    Implementation of initial inhabitant management at City level, methods for adding inhabitants by hand or by using a random function.

--- models/city.py
+++ models/city.py
@@ -1,11 +1,11 @@
 from models.buildings.building_type import BuildingType
-
+from inhabitant import Inhabitant
 
 class City:
     """
         Class City which manage all the management of each event of the day
     """
-    def __init__(self, name):
+    def __init__(self, name, arrivals=None):
         self.__name = name
         self.__list_building = []
         self.__stock = {
@@ -13,6 +13,7 @@ class City:
             "Electricity" : 0,
             "Food" : 0
         }
+        self.arrivals = arrivals if arrivals is not None else []
 
     @property
     def population(self):
@@ -41,3 +42,21 @@ class City:
     def next_turn(self):
         pass
 
+    def add_inhabitant(self, inhabitant):
+        """
+        Add an inhabitant to the city.
+        """
+        if not isinstance(inhabitant, Inhabitant):
+            raise TypeError("Only Inhabitant objects can be added. Use Inhabitant class methods to create them.")
+        self.arrivals.append(inhabitant)
+
+    def add_random_inhabitants(self, count):
+        """Add multiple random inhabitants to the city"""
+        for _ in range(count):
+            self.add_inhabitant(Inhabitant.create_random())
+
+    def create_and_add_random(self):
+        """Convenience method that creates and adds a random inhabitant"""
+        new_inhabitant = Inhabitant.create_random()
+        self.add_inhabitant(new_inhabitant)
+        return new_inhabitant
